450ab2dc485768566ce4f3586ea813177f7688ae
Kundan Suryansh <kundan.suryansh@ymedialabs.com>
2019-08-28 13:19:08 +0530

    getting the all station of train with arrival time is implemented

--- src/main/java/com/kundan/railticket/controller/StationController.java
+++ src/main/java/com/kundan/railticket/controller/StationController.java
@@ -1,17 +1,30 @@
 package com.kundan.railticket.controller;
 
 import com.kundan.railticket.dao.StationRepository;
+import com.kundan.railticket.dao.TrainRepository;
+import com.kundan.railticket.dao.TrainStationRepository;
 import com.kundan.railticket.entity.Station;
+import com.kundan.railticket.entity.TrainStation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Time;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class StationController {
 
     @Autowired
     StationRepository stationRepository;
+
+    @Autowired
+    TrainStationRepository trainStationRepository;
+
+    @Autowired
+    TrainRepository trainRepository;
+
     @GetMapping("/stations")
     List<Station> getAllStations()
     {
@@ -40,5 +53,17 @@ public class StationController {
             stationRepository.updateStationById(station.getStationId(),station.getName());
         }
         return getAllStations();
+    }
+    @GetMapping("/train-station/{trainNo}")
+    Map<String,String> getAllStationOfTrainWithArrivalTime(@PathVariable int trainNo)
+    {
+        Map<String,String> map=new HashMap<String, String>();
+
+        List<TrainStation> trainStations=trainStationRepository.getTrainStationByTrain(trainRepository.getTrainsByTrainNo(trainNo));
+        for(TrainStation trainStation:trainStations)
+        {
+            map.put(trainStation.getArrivalTime().toString(),trainStation.getStations().getName());
+        }
+        return map;
     }
 }
